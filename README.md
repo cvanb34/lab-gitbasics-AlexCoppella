3c0118b2374c0b82660131e93c085d94d08b602c
Dan Miller <TheDanMiller@users.noreply.github.com>
2019-08-30 10:07:46 -0400

    Initialize feature/empty_files

---
+++ README.md
@@ -0,0 +1,2 @@
+# GitBasicsLab
+First lab for SRU CPSC 317 students
---
+++ simpleMath.js
@@ -0,0 +1,18 @@
+
+function simpleAdd(number1, number2) {
+    // implement a function that adds two numbers and returns their values
+    return parseInt(number1) + parseInt(number2);
+}
+
+function absoluteValueSubtract(number1, number2) {
+    // implement a function that subtracts two numbers and returns the absolute value
+}
+
+function multiplyByTen(number) {
+    // implement a function that multiplies what is passed in by ten
+}
+
+function generateRandomNumber() {
+    // generate a random number between 1 and 42
+}
+
